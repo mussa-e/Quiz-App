71e9388690973445edd204556181af4db28c690a
Mussa Faragi <mussa.faragi@gmx.de>
2025-05-29 21:37:55 +0200

    added json array

---
+++ script.js
@@ -0,0 +1,67 @@
+let questions = [
+    {
+        "question" : "Wer hat HTML erfunden?", 
+        "answer_1" : "Robbie Williams", 
+        "answer_2" : "Lady Gaga", 
+        "answer_3" : "Tim Berners-Lee", 
+        "answer_4" : "Justin Bieber", 
+        "right_answer" : 3
+    },
+    {
+        "question" : "Wofür steht HTML?", 
+        "answer_1" : "Hypertext Markup Language", 
+        "answer_2" : "Hypertext Markup Link", 
+        "answer_3" : "Hypertext Manual Line", 
+        "answer_4" : "Hydrotext Mesure Link", 
+        "right_answer" : 1
+    },
+    {
+        "question" : "Wofür steht JSON?", 
+        "answer_1" : "Javascript ocure notation", 
+        "answer_2" : "Javascript online number", 
+        "answer_3" : "Javascript object notation", 
+        "answer_4" : "Justify sentence object number", 
+        "right_answer" : 3
+    },
+    {
+        "question" : "Wofür steht CSS?", 
+        "answer_1" : "Cascading stylesheet", 
+        "answer_2" : "cursor style system", 
+        "answer_3" : "cascading style system", 
+        "answer_4" : "census system style", 
+        "right_answer" : 1
+    },
+    {
+        "question" : "welches Tag ist für Überschriften?", 
+        "answer_1" : "div-tag", 
+        "answer_2" : "h-tag", 
+        "answer_3" : "p-tag", 
+        "answer_4" : "button-tag", 
+        "right_answer" : 2
+    },
+    {
+        "question" : "Mit welchem Befehl werden Funtionen auf buttons ausgeführt?", 
+        "answer_1" : "onload", 
+        "answer_2" : "onclick", 
+        "answer_3" : "onchange", 
+        "answer_4" : "ondrag", 
+        "right_answer" : 2
+    },
+    {
+        "question" : "Wie lautet die richtige Notation für Funktionen?", 
+        "answer_1" : "function beispiel();", 
+        "answer_2" : "function beispiel[];", 
+        "answer_3" : "function beispiel{};", 
+        "answer_4" : "function beispiel=;", 
+        "right_answer" : 1
+    },
+    {
+        "question" : "Was ist eine globale Variable?", 
+        "answer_1" : "Eine Variable innerhalb einer Funktion", 
+        "answer_2" : "Eine Varibale außerhalb einer Funktion", 
+        "answer_3" : "Eine unbestimmte Variable", 
+        "answer_4" : "Eine unbenannte Variable", 
+        "right_answer" : 2
+    }
+
+];
