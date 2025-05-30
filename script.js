7de579cf34368d0ee796b8382dbc9b7987624082
Mussa Faragi <mussa.faragi@gmx.de>
2025-05-30 20:22:54 +0200

    questions and answers functions

--- script.js
+++ script.js
@@ -65,3 +65,30 @@ let questions = [
     }
 
 ];
+
+
+let currentQuestion = 0;
+
+
+function init(){
+    document.getElementById("all-questions").innerHTML = questions.length;
+
+    showQuestion();
+}
+
+
+function showQuestion(){
+    let question = questions[currentQuestion];
+    
+    document.getElementById("questiontext").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+}
+
+
+function nextQuestion(){
+    currentQuestion++;
+    showQuestion();
+}
